3207de05430bda220e6a40ab4a28ed828a3577e5
Gustavo Pessa <ghmpessa@gmail.com>
2021-02-17 18:02:13 -0300

    fix: change webpack env var to use json.stringfy

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new DefinePlugin({
-      'process.env.API_URL': 'http://fordevs.herokuapp.com/ap'
+      'process.env.API_URL': JSON.stringify('http://fordevs.herokuapp.com/ap')
     })
   ]
 }
