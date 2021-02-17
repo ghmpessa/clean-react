62d4593b1347360e59fa20c05c6077cfa26328d0
Gustavo Pessa <ghmpessa@gmail.com>
2021-02-17 18:03:26 -0300

    feat: change Login composition to add SaveAccessToken

--- src/main/factories/usecases/authentication/remote-authentication-factory.ts
+++ src/main/factories/usecases/authentication/remote-authentication-factory.ts
@@ -1,4 +1,4 @@
-import { RemoteAuthentication } from '@/data/usercases/authentication/remote-authentication'
+import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication'
 import { makeAxiosHttpClient } from '../../http/axios-http-client-factory'
 import { Authentication } from '@/domain/usecases'
 import { makeApiUrl } from '../../http/api-url-factory'
