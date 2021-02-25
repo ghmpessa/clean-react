c5474bafd89ba50f44acf3e0a32dc116c67a7109
Gustavo Pessa <ghmpessa@gmail.com>
2021-02-25 17:26:48 -0300

    test: ensure Login present error message if invalid credentials are provided

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,8 +1,10 @@
 import faker from 'faker'
 
+const baseUrl: string = Cypress.config().baseUrl
+
 describe('Login', () => {
   beforeEach(() => {
-    cy.visit('login')
+    cy.visit('/login')
   })
 
   it('Should load with correct initial state', () => {
@@ -42,5 +44,17 @@ describe('Login', () => {
       .should('contain.text', '✅')
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error if invalid credentials are provided', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.internet.password())
+    cy.getByTestId('submit').click()
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner').should('exist')
+      .getByTestId('main-error').should('not.exist')
+      .getByTestId('spinner').should('not.exist')
+      .getByTestId('main-error').should('exist')
+    cy.url().should('eql', `${baseUrl}/login`)
   })
 })
