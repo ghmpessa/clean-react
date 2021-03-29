9ddfc4fbf80125118f33b24d717f2056154b8ab0
Gustavo Pessa <ghmpessa@gmail.com>
2021-03-29 11:32:42 -0300

    feat: add initial survey-list view

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { SurveyList } from '@/presentation/pages'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 type Factory = {
@@ -12,6 +13,7 @@ const Router: React.FC<Factory> = (factory: Factory) => {
       <Switch>
         <Route path="/login" exact component={factory.makeLogin}/>
         <Route path="/signup" exact component={factory.makeSignUp}/>
+        <Route path="/" exact component={SurveyList}/>
       </Switch>
     </BrowserRouter>
   )
