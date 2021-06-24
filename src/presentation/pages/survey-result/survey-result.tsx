99b4ccc74c531d3f8d536122c9e6205eec6cb119
Gustavo Pessa <ghmpessa@gmail.com>
2021-06-24 19:21:34 -0300

    feat: ensure SurveyResult reload on button click

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -16,8 +16,18 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     error: '',
-    surveyResult: null as LoadSurveyResult.Model
+    surveyResult: null as LoadSurveyResult.Model,
+    reload: false
   })
+
+  const reload = (): void => {
+    setState(old => ({
+      surveyResult: null,
+      error: '',
+      reload: !old.reload,
+      isLoading: false
+    }))
+  }
 
   useEffect(() => {
     loadSurveyResult.load()
@@ -25,7 +35,7 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
         setState(old => ({ ...old, surveyResult }))
       })
       .catch(handleError)
-  }, [])
+  }, [state.reload])
 
   return (
     <div className={Styles.surveyResultWrap}>
@@ -53,7 +63,7 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
           </>
         }
         {state.isLoading && <Loading />}
-        {state.error && <Error error={state.error} reload={() => {}} />}
+        {state.error && <Error error={state.error} reload={reload} />}
       </div>
       <Footer />
     </div>
