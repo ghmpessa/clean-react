726267b43f0bda34e0189ac17229345e891cbd73
Gustavo Pessa <ghmpessa@gmail.com>
2021-06-25 14:47:00 -0300

    refactor: change HttpClient to be more generic

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,6 +1,6 @@
 import { AxiosHttpClient } from './axios-http-client'
 import { mockAxios, mockHttpResponse } from '@/infra/test'
-import { mockPostRequest ,mockGetRequest } from '@/data/test'
+import { mockHttpRequest } from '@/data/test'
 import axios from 'axios'
 
 jest.mock('axios')
@@ -23,18 +23,23 @@ const makeSut = (): SutTypes => {
 describe('AxiosHttpClient', () => {
   describe('post', () => {
     test('Should call axios.post with correct values', async () => {
-      const request = mockPostRequest()
+      const request = mockHttpRequest()
       const { sut, mockedAxios } = makeSut()
-      await sut.post(request)
-      expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+      await sut.request(request)
+      expect(mockedAxios.request).toHaveBeenCalledWith({
+        url: request.url,
+        data: request.body,
+        method: request.method,
+        headers: request.headers
+      })
     })
 
     // Estamos comparando uma promise com outra promise, por isso retiramos o async da função
     // e o await da variável
     test('Should call correct response on axios.post', async () => {
       const { sut, mockedAxios } = makeSut()
-      const httpResponse = await sut.post(mockGetRequest())
-      const axiosResponse = await mockedAxios.post.mock.results[0].value // results[0]: resolved
+      const httpResponse = await sut.request(mockHttpRequest())
+      const axiosResponse = await mockedAxios.request.mock.results[0].value // results[0]: resolved
       expect(httpResponse).toEqual({
         statusCode: axiosResponse.status,
         body: axiosResponse.data
@@ -46,36 +51,8 @@ describe('AxiosHttpClient', () => {
       mockedAxios.post.mockRejectedValueOnce({
         response: mockHttpResponse()
       })
-      const promise = sut.post(mockPostRequest())
-      expect(promise).toEqual(mockedAxios.post.mock.results[0].value) // results[0]: resolved
-    })
-  })
-
-  describe('get', () => {
-    test('Should call axios.get with correct values', async () => {
-      const request = mockGetRequest()
-      const { sut, mockedAxios } = makeSut()
-      await sut.get(request)
-      expect(mockedAxios.get).toHaveBeenCalledWith(request.url, { headers: request.headers })
-    })
-
-    test('Should call correct response on axios.get', async () => {
-      const { sut, mockedAxios } = makeSut()
-      const httpResponse = await sut.get(mockGetRequest())
-      const axiosResponse = await mockedAxios.get.mock.results[0].value
-      expect(httpResponse).toEqual({
-        statusCode: axiosResponse.status,
-        body: axiosResponse.data
-      })
-    })
-
-    test('Should call correct error on axios.get', () => {
-      const { sut, mockedAxios } = makeSut()
-      mockedAxios.get.mockRejectedValueOnce({
-        response: mockHttpResponse()
-      })
-      const promise = sut.get(mockGetRequest())
-      expect(promise).toEqual(mockedAxios.get.mock.results[0].value) // results[0]: resolved
+      const promise = sut.request(mockHttpRequest())
+      expect(promise).toEqual(mockedAxios.request.mock.results[0].value) // results[0]: resolved
     })
   })
 })
--- src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -1,8 +1,8 @@
 import { LoadSurveyList } from '@/domain/usecases'
 import { makeApiUrl } from '../../http/api-url-factory'
 import { RemoteLoadSurveyList } from '@/data/usecases/load-survey-list/remote-load-survey-list'
-import { makeAuthorizeHttpGetClientDecorator } from '@/main/factories/decorators'
+import { makeAuthorizeHttpClientDecorator } from '@/main/factories/decorators'
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
-  return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAuthorizeHttpGetClientDecorator())
+  return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAuthorizeHttpClientDecorator())
 }
