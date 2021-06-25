1c006046334c175df5416b7464aa664258c2d58c
Gustavo Pessa <ghmpessa@gmail.com>
2021-06-25 11:34:03 -0300

    test: ensure SurveyResult presents error if request fails

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -1,7 +1,19 @@
 import * as Helper from '../utils/helpers'
+import * as Http from '../utils/http-mocks'
 
-describe('SurveyList', () => {
-  it('Should logout if survey-result has no token', () => {
+const path = /surveys/
+const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET')
 
+describe('SurveyResult', () => {
+  beforeEach(() => {
+    cy.fixture('account').then(account => {
+      Helper.setLocalStorageItem('account', account)
+    })
+  })
+
+  it('Should present error on UnexpectedError', () => {
+    mockUnexpectedError()
+    cy.visit('/surveys/any_id')
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente mais tarde.')
   })
 })
