f9eb5ea0fe50809d75b2ae23623537e5df531ba6
Gustavo Pessa <ghmpessa@gmail.com>
2021-06-28 11:12:03 -0300

    refactor: remove flip move

--- src/presentation/pages/survey-result/components/result/result.tsx
+++ src/presentation/pages/survey-result/components/result/result.tsx
@@ -4,7 +4,6 @@ import { Calendar } from '@/presentation/components'
 import { SurveyResultAnswer } from '@/presentation/pages/survey-result/components'
 import { LoadSurveyResult } from '@/domain/usecases'
 import { useHistory } from 'react-router-dom'
-import FlipMove from 'react-flip-move'
 
 type Props = {
   surveyResult: LoadSurveyResult.Model
@@ -18,11 +17,9 @@ const Result: React.FC<Props> = ({ surveyResult }: Props) => {
         <Calendar date={surveyResult.date} className={Styles.calendarWrap}/>
         <h2 data-testid='question'>{surveyResult.question}</h2>
       </hgroup>
-      <FlipMove data-testid='answers' className={Styles.answersList}>
-        <>
-          {surveyResult.answers.map(answer => (<SurveyResultAnswer key={answer.answer} answer={answer} />))}
-        </>
-      </FlipMove>
+      <ul data-testid='answers' className={Styles.answersList}>
+        {surveyResult.answers.map(answer => (<SurveyResultAnswer key={answer.answer} answer={answer} />))}
+      </ul>
       <button data-testid='back-button' onClick={goBack}>Voltar</button>
     </>
   )
